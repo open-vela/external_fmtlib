c6c726d3abca8af4fc13a4f06d40bcdac9f44d17
openvela-robot <openvela-robot@xiaomi.com>
2020-10-27 13:55:26 +0800

    Workaround bugs in gcc 8

--- include/fmt/compile.h
+++ include/fmt/compile.h
@@ -668,14 +668,13 @@ OutputIt format_to(OutputIt out, const S&, const Args&... args) {
 }
 
 template <typename OutputIt, typename CompiledFormat, typename... Args>
-auto format_to_n(OutputIt out, size_t n, const CompiledFormat& cf,
-                 const Args&... args) ->
-    typename std::enable_if<
-        detail::is_output_iterator<OutputIt,
-                                   typename CompiledFormat::char_type>::value &&
-            std::is_base_of<detail::basic_compiled_format,
-                            CompiledFormat>::value,
-        format_to_n_result<OutputIt>>::type {
+typename std::enable_if<(detail::is_output_iterator<
+                         OutputIt, typename CompiledFormat::char_type>::value&&
+                             std::is_base_of<detail::basic_compiled_format,
+                                             CompiledFormat>::value),
+                        format_to_n_result<OutputIt>
+>::type format_to_n(OutputIt out, size_t n,
+                    const CompiledFormat& cf, const Args&... args) {
   auto it =
       format_to(detail::truncating_iterator<OutputIt>(out, n), cf, args...);
   return {it.base(), it.count()};
